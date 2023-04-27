9c67e5b75fc92daabcbe7adc3cbb9a72f71b5b29
John T. Wodder II <git@varonathe.org>
2023-04-26 23:06:01 -0400

    Fix docs build

--- src/reformations.rs
+++ src/reformations.rs
@@ -5,8 +5,8 @@ pub const GREGORIAN: JulianDayT = 2299161; // noon on 1582-10-15
 pub const UNITED_KINGDOM: JulianDayT = 2361222; // noon on 1752-09-14
 
 /// The smallest Julian day number that can be passed to
-/// [`Calendar::reforming()`] without getting an [`Error::InvalidReformation`]
-/// error.
+/// [`Calendar::reforming()`][crate::Calendar::reforming] without getting an
+/// [`Error::InvalidReformation`][crate::Error::InvalidReformation] error.
 ///
 /// This Julian day number corresponds to the date 0300-03-01 N.S. (0300-02-29
 /// O.S.).
